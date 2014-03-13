d1308cd4ffc3bfb73bb56a9d39b9b56e44cb695d
fen <fen@justfen.com>
2014-03-13 18:03:04 +0000

    Proper Unicode Error handling & replies

--- ud.py
+++ ud.py
@@ -11,22 +11,26 @@ from willie import web
 from willie.module import commands, example
 import urllib
 import json
+import sys
 
 @commands('ud')
 @example('.ud getme')
 def ud_search(bot, trigger):
     query = trigger.group(2)
     url = 'http://api.urbandictionary.com/v0/define?term=%s' %(query)
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
+    try:
+      response = urllib.urlopen(url)
+    except UnicodeError:
+      bot.reply('ENGLISH MOTHERFUCKER, DO YOU SPEAK IT?')
+      exit(0)
+    else:
+      data = json.loads(response.read())
     try:
       definition = data['list'][0]['definition']
     except KeyError:
-      bot.say('Something went wrong brah')
+      bot.reply('Something went wrong brah')
     except IndexError:
-      bot.say('No results, do you even spell brah?')
-    except UnicodeError:
-      bot.say('(╯°□°）╯︵ ┻━┻) ASCII ONLY!!!!')
+      bot.reply('No results, do you even spell brah?')
     else:
       thumbsup = data['list'][0]['thumbs_up']
       thumbsdown = data['list'][0]['thumbs_down']
