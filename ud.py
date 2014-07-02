7e26d4edf5235b959893c4b43ed597cff4f7cd6a
undernocircumstance <simon@iamsimon.co.uk>
2014-07-02 11:44:10 +0100

    fix: another attempt at ud.py

--- ud.py
+++ ud.py
@@ -17,11 +17,12 @@ import sys
 @example('.ud getme')
 def ud_search(bot, trigger):
     query = trigger.group(2)
-    if "spam" in query:
+    querym = query.replace(" ", "")
+    if "spam" in querym:
       bot.reply('Negative ghostrider')
       exit(0)
     else:
-      url = 'http://api.urbandictionary.com/v0/define?term=%s' %(query)
+      url = 'http://api.urbandictionary.com/v0/define?term=%s' %(querym)
     try:
       response = urllib.urlopen(url)
     except UnicodeError:
