645b2916a918aad241a0309a6c343e5cfd3dea89
fen <justfenyo@gmail.com>
2024-12-01 21:47:39 +0000

    feat: to python3, sopel and adding tests.

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 # urbandic-module
 
-Little bit of python for use with the Willie IRC bot @  http://willie.dftba.net/ & https://github.com/embolalia/willie
+Python module for use with the Sopel IRC bot (formerly known as Willie) @ https://sopel.chat
 
 # What does it do?
 
-Simply, throw a word at it using the .ud command and you'll get the top defintion back from http://www.urbandictionary.com/
+Simply, throw a word at it using the `.ud` command and you'll get the top definition back from http://www.urbandictionary.com/. Use `.ud2` to get the second definition.
 
 ## But whyyyy?
 
 This was knocked together to help the yanks understand the brits in #crude on irc.freenode.net. Getme bruv.
 
+# Version Compatibility
+
+- Python 3.x compatible
+- Works with Sopel IRC bot (formerly Willie)
+
 # How to install?
 
-1. Clone the python module to the modules folder for your willie install. This will usally be `/home/YOURUSERNAME/.willie`
-2. Make sure your `default.cfg` file (Same dir) contains the line `extra = /home/YOURUSERNAME/.willie/modules`
+1. Clone the python module to the modules folder for your Sopel install. This will usually be:
+   - Linux: `~/.sopel/modules`
+   - Windows: `%APPDATA%\sopel\modules`
+2. Make sure your `default.cfg` file (Same dir) contains the line:
+   - Linux: `extra = ~/.sopel/modules`
+   - Windows: `extra = %APPDATA%\sopel\modules`
+
+# Commands
+
+- `.ud <word>` - Get the top Urban Dictionary definition
+- `.ud2 <word>` - Get the second most popular definition
--- ud.py
+++ ud.py
@@ -1,76 +1,111 @@
 """
 ud.py - A communication tool to aid Europe and USA understand each other in #crude on irc.freenode.net
-Copyright 2014, justFen
 Licensed under the Eiffel Forum License 2.
-
-http://justfen.com
 """
 
 import re
-from willie import web
-from willie.module import commands, example
-import urllib
+from sopel import web
+from sopel.module import commands, example
+import urllib.request
 import json
 import sys
+
+def split_message(message, max_length=400):
+    """Split a message into multiple parts if it exceeds max_length."""
+    parts = []
+    while message:
+        if len(message) <= max_length:
+            parts.append(message)
+            break
+        
+        # Find the last space before max_length
+        split_point = message.rfind(' ', 0, max_length)
+        if split_point == -1:  # No space found, force split at max_length
+            split_point = max_length
+        
+        parts.append(message[:split_point])
+        message = message[split_point:].lstrip()
+    
+    return parts
 
 @commands('ud')
 @example('.ud getme')
 def ud_search(bot, trigger):
     query = trigger.group(2)
+    if query is None:
+        bot.reply('Please provide a word to look up')
+        return
     if "spam" in query:
-      bot.reply('Negative ghostrider')
-      exit(0)
-    else:
-      url = 'http://api.urbandictionary.com/v0/define?term=%s' %(query)
+        bot.reply('Negative ghostrider')
+        return
+    
+    url = 'http://api.urbandictionary.com/v0/define?term={}'.format(query)
     try:
-      response = urllib.urlopen(url)
+        response = urllib.request.urlopen(url)
     except UnicodeError:
-      bot.reply('ENGLISH MOTHERFUCKER, DO YOU SPEAK IT?')
-      exit(0)
-    else:
-      data = json.loads(response.read())
+        bot.reply('ENGLISH MOTHERFUCKER, DO YOU SPEAK IT?')
+        return
+    
+    data = json.loads(response.read().decode('utf-8'))
     try:
-      definition = data['list'][0]['definition']
-    except KeyError:
-      bot.reply('Something went wrong brah')
-    except IndexError:
-      bot.reply('No results, do you even spell brah?')
-    else:
-      thumbsup = data['list'][0]['thumbs_up']
-      thumbsdown = data['list'][0]['thumbs_down']
-      udoutput = "Definition; %s >> Up %s Down %s" % (definition,thumbsup,thumbsdown)
-      if not "spam spam" in udoutput:
-          bot.reply(udoutput)
-      else:
-          bot.reply('Negative ghostrider')
+        definition = data['list'][0]['definition']
+    except (KeyError, IndexError):
+        bot.reply('No results, do you even spell brah?')
+        return
+    
+    thumbsup = data['list'][0]['thumbs_up']
+    thumbsdown = data['list'][0]['thumbs_down']
+    udoutput = "Definition; {} >> Up {} Down {}".format(definition, thumbsup, thumbsdown)
+    
+    if "spam spam" in udoutput:
+        bot.reply('Negative ghostrider')
+        return
+        
+    # Split and send message in parts if needed
+    message_parts = split_message(udoutput)
+    for i, part in enumerate(message_parts, 1):
+        if len(message_parts) > 1:
+            bot.reply('[{}/{}] {}'.format(i, len(message_parts), part))
+        else:
+            bot.reply(part)
 
 @commands('ud2')
 @example('.ud2 getme')
 def ud_search_1(bot, trigger):
     query = trigger.group(2)
+    if query is None:
+        bot.reply('Please provide a word to look up')
+        return
     if "spam" in query:
-      bot.reply('Negative ghostrider')
-      exit(0)
-    else:
-      url = 'http://api.urbandictionary.com/v0/define?term=%s' %(query)
+        bot.reply('Negative ghostrider')
+        return
+    
+    url = 'http://api.urbandictionary.com/v0/define?term={}'.format(query)
     try:
-      response = urllib.urlopen(url)
+        response = urllib.request.urlopen(url)
     except UnicodeError:
-      bot.reply('ENGLISH MOTHERFUCKER, DO YOU SPEAK IT?')
-      exit(0)
-    else:
-      data = json.loads(response.read())
+        bot.reply('ENGLISH MOTHERFUCKER, DO YOU SPEAK IT?')
+        return
+    
+    data = json.loads(response.read().decode('utf-8'))
     try:
-      definition = data['list'][1]['definition']
-    except KeyError:
-      bot.reply('Something went wrong brah')
-    except IndexError:
-      bot.reply('No results, do you even spell brah?')
-    else:
-      thumbsup = data['list'][1]['thumbs_up']
-      thumbsdown = data['list'][1]['thumbs_down']
-      udoutput = "Definition; %s >> Up %s Down %s" % (definition,thumbsup,thumbsdown)
-      if not "spam spam" in udoutput:
-          bot.reply(udoutput)
-      else:
-          bot.reply('Negative ghostrider')
+        definition = data['list'][1]['definition']
+    except (KeyError, IndexError):
+        bot.reply('No results, do you even spell brah?')
+        return
+    
+    thumbsup = data['list'][1]['thumbs_up']
+    thumbsdown = data['list'][1]['thumbs_down']
+    udoutput = "Definition; {} >> Up {} Down {}".format(definition, thumbsup, thumbsdown)
+    
+    if "spam spam" in udoutput:
+        bot.reply('Negative ghostrider')
+        return
+        
+    # Split and send message in parts if needed
+    message_parts = split_message(udoutput)
+    for i, part in enumerate(message_parts, 1):
+        if len(message_parts) > 1:
+            bot.reply('[{}/{}] {}'.format(i, len(message_parts), part))
+        else:
+            bot.reply(part)
